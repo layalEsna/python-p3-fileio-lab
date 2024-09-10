b694266d09cb6098ba3113ea2bccb03aae02da89
Amene <layla_1380@yahoo.com>
2024-09-10 14:08:01 -0500

    read write append methods

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name , file_content):
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name+= '.txt'
+    with open(file_name, 'w') as f:
+       f.write(file_content) 
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name+= '.txt'
+    with open(file_name, 'a') as f:
+        f.write(append_content)
+
+
+ 
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+    with open(file_name, 'r') as f:
+        return f.read()
